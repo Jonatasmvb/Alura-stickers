e0593063036a3445a8c8bf13af158572290d4e08
Jônatas <jonatasmvb@gmail.com>
2022-07-19 16:16:58 -0300

    primeiro dia sem alteracoes

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Fazer uma conexão HTTP e Receber os dados da API
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // Filtrar somente os dados que interessam (Titulo, Poster, Classificação)
+        var parser = new JsonParser();
+        List<Map<String,String>> ListaDeFilmes = parser.parse(body);
+
+        //Exibir manipular os dados
+        for (Map<String,String> filme : ListaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
+    }
+}
